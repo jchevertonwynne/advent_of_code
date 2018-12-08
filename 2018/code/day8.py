45f88e7ce372b49996536e88bd0da9fee31b9e04
j.cheverton-wynne <joseph.cheverton-wynne@bjss.com>
2018-12-08 16:03:37 +0000

    moved behaviour to node class

--- 2018/code/day8.py
+++ 2018/code/day8.py
@@ -15,22 +15,24 @@ class Node:
     def __repr__(self):
         return f"Node(ind: {self.ind}, metadata items:{self.data_len}, children:{len(self.children)})"
 
-def setup():
+    def sum(self):
+        return sum(self.data_vals) + sum(child.sum() for child in self.children)
+
+def read_file():
     with open(input_filename) as f:
         return [int(i) for i in f.read().split(' ')]
 
-def part1(nums):
-    stack = []
+def setup():
+    nums = read_file()
+    base = Node(0, nums[0], nums[1])
     total = 0
     ind = 0
-    base = Node(0, nums[0], nums[1])
-    stack.append(base)
+    stack = [base]
     while stack:
         top = stack[-1]
         if top.child_remaining == 0:
             stack.pop()
             top.data_vals = nums[(ind + 2):(ind + top.data_len + 2)]
-            total += sum(top.data_vals)
             ind += top.data_len
         else:
             ind += 2
@@ -38,20 +40,23 @@ def part1(nums):
             stack.append(next)
             top.child_remaining -= 1
             top.children.append(next)
-    return base, total
+    return base
 
-def part2(node):
-    if not node.children:
-        return sum(node.data_vals)
-    return sum(part2(node.children[data_val - 1]) for data_val in node.data_vals if data_val < len(node.children) + 1)
+def part1(tree):
+    return tree.sum()
+
+def part2(tree):
+    if not tree.children:
+        return sum(tree.data_vals)
+    return sum(part2(tree.children[data_val - 1]) for data_val in tree.data_vals if data_val < len(tree.children) + 1)
 
 def main():
     start_setup = time.time()
-    nums = setup()
+    base = setup()
     end_setup = time.time()
 
     start_part1 = time.time()
-    base, res_part1 = part1(nums)
+    res_part1 = part1(base)
     end_part1 = time.time()
 
     start_part2= time.time()
